b13f536c130fa3a01af8cebc8491b59147d9b8ae
Loganathan <loganathan.palani@yahoo.com>
2026-07-27 23:21:16 +0530

    Reverted scalar api reference implementation

--- src/shared/swagger/swagger.ts
+++ src/shared/swagger/swagger.ts
@@ -1,15 +1,19 @@
-import { ApiReference } from "@scalar/api-reference";
-import type { Express, RequestHandler } from "express";
-import fs from "node:fs";
+import type { Express } from "express";
 import path from "node:path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import fs from "node:fs";
 
 export const configureSwagger = (app: Express): void => {
-  const openApiPath = path.join(process.cwd(), "docs", "openapi.yaml");
+  const swaggerPath = path.join(process.cwd(), "docs", "openapi.yaml");
 
-  const specification = fs.readFileSync(openApiPath, "utf8");
+  console.log("cwd =", process.cwd());
+  console.log("swaggerPath =", swaggerPath);
+  console.log("exists =", fs.existsSync(swaggerPath));
 
-  app.use(
-    "/api-docs",
-    ApiReference as unknown as RequestHandler
-  );
+  const swaggerDocument = YAML.load(swaggerPath);
+
+  //console.log(`Swagger Path: ${swaggerPath}`);
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
